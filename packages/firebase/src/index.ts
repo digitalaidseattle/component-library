72b0acb17c9ef862ed75790a6fb5088189377b23
jnakaso <jnakaso@yahoo.com>
2026-03-11 09:08:21 -0700

    minor compile fixes

--- packages/firebase/src/index.ts
+++ packages/firebase/src/index.ts
@@ -2,4 +2,3 @@ export * from "./FirebaseAuthService";
 export * from "./firebaseClient";
 export * from "./FirebaseStorageService";
 export * from "./FirestoreService";
-export * from "./GeminiService";
